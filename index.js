0f5735c6c5f679a708d5661b55013faad817dc4f
dsone <dsone@users.noreply.github.com>
2021-04-23 21:51:33 +0200

    Remove unused IPC and EventEmitter

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 
 function createWindow() {
-	// Erstelle das Browser-Fenster.
 	let win = new BrowserWindow({
 		width: 1500, height: 800,
 		minWidth: 800, minHeight: 600,
@@ -11,8 +10,7 @@ function createWindow() {
 		},
 		icon: __dirname + '/www/favicon.ico'
 	});
-	
-	// und lade die index.html der App.
+
 	win.loadFile('./www/index.html');
 	win.setAutoHideMenuBar(true);
 
@@ -32,35 +30,14 @@ app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// Unter macOS ist es üblich, für Apps und ihre Menu Bar
-	// aktiv zu bleiben, bis der Nutzer explizit mit Cmd + Q die App beendet.
 	if (process.platform !== 'darwin') {
 		app.quit();
 	}
 });
 
 app.on('activate', () => {
-	// Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
-	// das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
+	// macOS dock icon click
 	if (BrowserWindow.getAllWindows().length === 0) {
 		createWindow();
 	}
-});
-
-const ipc = require('electron').ipcMain;
-/*ipc.on('synMessage', (event, args) => {
-	console.log(args);
-	event.returnValue = 'Main said I received your Sync message';
-});*/
-
-ipc.on('filePick', (event, filePath) => {
-	console.log(`${ (new Date()).toLocaleTimeString() }: Event filePick received,`, filePath);
-
-	event.sender.send('rFilePick', { error: true, message: '' });
-});
-
-ipc.on('tablePick', (event, tableName) => {
-	console.log(`${ (new Date()).toLocaleTimeString() }: Event tablePick received,`, tableName);
-
-
 });
